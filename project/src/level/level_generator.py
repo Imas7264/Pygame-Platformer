6bf5ebeb404745e9034048806d12b0fd87ba44a3
Sami Ansari <samiansariwork@gmail.com>
2026-06-04 07:51:30 +0530

    Implemented generation of reachable platforms in a single direction(path: left to right)

--- project/src/level/level_generator.py
+++ project/src/level/level_generator.py
@@ -4,7 +4,7 @@ from settings import WINDOW_HEIGHT, WINDOW_WIDTH, TILE_SIZE
 WIDTH = WINDOW_WIDTH // TILE_SIZE
 HEIGHT = WINDOW_HEIGHT // TILE_SIZE
 
-def generate_level(level_number = 1):
+def generate_level_random(level_number = 1):
  level = []
 
  #Boundary generation
@@ -34,9 +34,57 @@ def generate_level(level_number = 1):
   for i in range(platform_length):
    level[platform_y][platform_x + i] = "G"
 
- #Enemy generation
- enemy_count = random.randint(platform_count-2, platform_count+2)
+ print("Total platforms: ", platform_count)
+ level = ["".join(row) for row in level]
 
+ return level
+
+
+def generate_level_path():
+ level = []
+
+ #Boundary generation
+ for row in range(HEIGHT):
+  if row == 0 or row == HEIGHT-1:
+   level.append("#" * WIDTH)
+  else:
+   level.append("#" + (" " * (WIDTH-2)) + "#")
+
+ level = [list(row) for row in level]
+
+ player_x, player_y = 2, HEIGHT - 5
+ level[player_y][player_x] = "P"
+
+ floor_row = HEIGHT - 3
+ floor_length = 4
+ for col in range(player_x, player_x+floor_length):
+  level[floor_row][col] = "G"
+
+ #Platform generation
+ platform_count = random.randint(5, 12)
+ current_x = player_x
+ current_y = player_y+2
+ current_length = floor_length
+
+ for _ in range(platform_count):
+  dx = random.randint(2,4)
+  dy = random.randint(-2,2)
+
+  platform_length = random.randint(1,4)
+  platform_x = current_x + dx + current_length
+  platform_y = current_y + dy
+
+  platform_y = max(3, min(HEIGHT-5, platform_y))
+
+  if(platform_x + platform_length) > WIDTH-2:
+   break
+  
+  for i in range(platform_length):
+   level[platform_y][platform_x + i] = "G"
+
+  current_x = platform_x
+  current_y = platform_y
+  current_length = platform_length
 
  print("Total platforms: ", platform_count)
  level = ["".join(row) for row in level]
